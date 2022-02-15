3f53a1fbb3eb725e1784d9ae9e22fa8a0e34cbd7
Ryan Wheeler <ryan.wheeler12@gmail.com>
2022-02-15 11:28:40 -0800

    Working on product cards

--- src/Components/RelatedItemsAndComparisons/RelatedProductCards.jsx
+++ src/Components/RelatedItemsAndComparisons/RelatedProductCards.jsx
@@ -1,1 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import FiveStar from '../FiveStar.jsx';
+
+const testAsset = '../../../../test-assets/images/product.jpeg';
+
+const ratings = {
+  1: '1',
+  2: '2',
+  3: '8',
+  4: '2',
+  5: '4',
+};
+
+const CardStyle = styled.div`
+  border: solid;
+`;
+
+function ProductCard() {
+  return (
+    <CardStyle className="product__card">
+      <div>Star</div>
+      <img src={testAsset} alt="placeholder for Product img" />
+      <div className="product__info">
+        <div>CATEGORY</div>
+        <div>Expanded Product Name with Extra Text</div>
+        <div>$123</div>
+        <div className="product__rating">
+          <FiveStar ratings={ratings} />
+        </div>
+      </div>
+    </CardStyle>
+  );
+}
+
+export default ProductCard;
+
+
+
+// Root div of Card will be clickable
--- src/Components/RelatedItemsAndComparisons/index.jsx
+++ src/Components/RelatedItemsAndComparisons/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colors, styles } from '../../styles.js';
+import RelatedProductCards from './RelatedProductCards.jsx';
 
 const RelatedStyle = styled.div`
   ${styles.Standard};
@@ -9,7 +10,9 @@ const RelatedStyle = styled.div`
 
 function RelatedItemsAndComparisons() {
   return (
-    <RelatedStyle className="relatedAndComparisons">RELATED ITEMS AND COMPARISONS WILL GO HERE</RelatedStyle>
+    <RelatedStyle className="relatedAndComparisons">
+      <RelatedProductCards />
+    </RelatedStyle>
   );
 }
 
