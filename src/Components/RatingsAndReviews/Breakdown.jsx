5b68305f9cd0397a5beff934a3e8c4179bb8b599
Owen Crookston <owen.crookston@gmail.com>
2022-02-14 18:32:38 -0800

    breakdown bars without css and without review filtering

--- src/Components/RatingsAndReviews/Breakdown.jsx
+++ src/Components/RatingsAndReviews/Breakdown.jsx
@@ -4,15 +4,33 @@ import PropTypes from 'prop-types';
 function Breakdown({ ratings }) {
   // Take in a ratings object and return five bars (one for each numerical rating)
   // Each bar shows proportinally (green) how many of the reviews had this value
+  const numberOfRatings = Object.values(ratings);
+  const total = numberOfRatings.reduce((p, c) => p + Number(c), 0);
+
   return (
-    <RatingBar />
+    <div className="breakdown">
+      <RatingBar className="rating-bar" stars="1" percent={((Number(ratings[1]) * 100) / total).toString()} />
+      <RatingBar className="rating-bar" stars="2" percent={((Number(ratings[2]) * 100) / total).toString()} />
+      <RatingBar className="rating-bar" stars="3" percent={((Number(ratings[3]) * 100) / total).toString()} />
+      <RatingBar className="rating-bar" stars="4" percent={((Number(ratings[4]) * 100) / total).toString()} />
+      <RatingBar className="rating-bar" stars="5" percent={((Number(ratings[5]) * 100) / total).toString()} />
+    </div>
   );
 }
 
-function RatingBar(props) {
+function RatingBar({ stars, percent }) {
   return (
     <div>
-      <h3>{props.number}</h3>
+      <label htmlFor="test">
+        {stars}
+        {' '}
+        Stars
+        {' '}
+      </label>
+      <progress id="test" max="100" value={percent}>
+        {percent}
+        %
+      </progress>
     </div>
   );
 }
@@ -26,3 +44,18 @@ Breakdown.propTypes = {
     5: PropTypes.string,
   }).isRequired,
 };
+
+RatingBar.propTypes = {
+  stars: PropTypes.string.isRequired,
+  percent: PropTypes.string.isRequired,
+};
+
+export default Breakdown;
+// Example
+// "ratings": {
+//   "1": "1",
+//   "2": "2",
+//   "3": "2",
+//   "4": "2",
+//   "5": "21"
+// }
