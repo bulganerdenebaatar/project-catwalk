828b80fadf76e580acb2d0515dacfaa623f2bbcb
Daniel Reinhuber <dreinhuber@gmail.com>
2022-02-16 11:58:48 -0600

    finished setting up the server routes

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const requests = require('./api_requests.js');
 
-router.get('/products', requests.getAllProducts);
+router.get('/products*', requests.getInfo);
+router.get('/reviews/*', requests.getInfo);
+router.get('/qa/*', requests.getInfo);
+router.get('/cart', requests.getInfo);
+
+router.post('/reviews/*', requests.postInfo);
+router.post('/qa/*', requests.postInfo);
+router.post('/cart', requests.postInfo);
+
+router.put('/reviews/*', requests.updateInfo);
+router.put('/qa/*', requests.updateInfo);
+
+
+
 
 module.exports = router;
