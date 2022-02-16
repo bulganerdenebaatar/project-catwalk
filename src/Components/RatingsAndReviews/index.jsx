2fb5ba875eb1ac870f16c72bce18eee773990269
Owen Crookston <owen.crookston@gmail.com>
2022-02-16 13:38:08 -0800

    basic reviews list placeholders

--- src/Components/RatingsAndReviews/index.jsx
+++ src/Components/RatingsAndReviews/index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors, styles } from '../../styles.js';
 import RatingsBreakdown from './RatingsBreakdown.jsx';
+import ReviewsList from './ReviewsList.jsx';
 
 const RandRStyle = styled.div`
   ${styles.Standard};
+  display: grid;
+  grid-template-columns 1fr 2fr;
   height: fit-content;
   width: 100%;
 `;
@@ -13,6 +16,7 @@ function RatingsAndReviews() {
   return (
     <RandRStyle className="ratingsAndReviews">
       <RatingsBreakdown />
+      <ReviewsList />
     </RandRStyle>
   );
 }
