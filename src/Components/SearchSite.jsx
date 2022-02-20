692b4a911ac892652d1eff30091fc9dcf7b8a605
Daniel Reinhuber <dreinhuber@gmail.com>
2022-02-19 20:30:02 -0600

    implemented a search function that successfully returns a new ID for a given key-word

---
+++ src/Components/SearchSite.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import { colors } from '../styles.js';
+import { GlobalContext } from '../App.jsx';
+
+const SearchBox = styled.form`
+  display: flex;
+  flex-direction: row;
+  height: 30px;
+  position: relative;
+  bottom: -25px;
+
+  button {
+    background-color: ${colors.standardBGColor};
+    border: none;
+    border-radius: 5px;
+    color: ${colors.standardTxtColor};
+    width: content-min;
+
+    :hover {
+      background-color: ${colors.standardComponentBG};
+      color: white;
+    }
+  }
+  input {
+    background-color: rgba(200, 255, 255, 0.1);
+    border: none;
+    padding-left: 10px;
+    border-radius: 10px;
+    color: ${colors.standardTxtColor};
+    font-family: inherit;
+  }
+`;
+
+function SearchSite() {
+  const { setProductId } = useContext(GlobalContext);
+  const [siteSearchInput, setSiteSearchInput] = useState('');
+
+  const handleSearchInputChange = (e) => {
+    setSiteSearchInput(e.target.value);
+  };
+
+  const handleSearchSiteClick = (e) => {
+    e.preventDefault();
+    console.log(siteSearchInput);
+    axios.get('shopdata/products?count=all')
+      .then((res) => {
+        console.log(res.data);
+        const newProduct = res.data.find((product) => product.name.toLowerCase().includes(siteSearchInput));
+        const newId = newProduct.id;
+        setProductId(newId);
+        setSiteSearchInput('');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <SearchBox onSubmit={handleSearchSiteClick}>
+      <input placeholder="Search Site..." type="Search" onChange={handleSearchInputChange} />
+      <button type="submit">Search</button>
+    </SearchBox>
+  );
+}
+
+export default SearchSite;
