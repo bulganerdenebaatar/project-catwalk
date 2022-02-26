c7d9cc0af44c72a258fbfcd5abe5a682f7b224ee
Daniel Reinhuber <dreinhuber@gmail.com>
2022-02-26 14:22:14 -0600

    when an item is on sale the original price now shows up in red and crossed out

--- src/Components/Overview/OverviewComponents/ProductInfo.jsx
+++ src/Components/Overview/OverviewComponents/ProductInfo.jsx
@@ -27,6 +27,16 @@ const ProductInformationStyle = styled.div`
     margin-top: 10px;
     padding: 0;
   }
+
+  p {
+    margin: none;
+  }
+
+  em {
+    color: red;
+    font-size: 0.75em;
+    text-decoration: line-through;
+  }
 `;
 
 function ProductInformation() {
@@ -40,8 +50,9 @@ function ProductInformation() {
       <FiveStar className="overview-info stars" rating={closestQuarter} />
       <h4 className="overview-info category">{productInfo.category}</h4>
       <h3 className="overview-info price">
-        $
-        {salePrice || productInfo.default_price}
+        { salePrice
+          ? (<p>${salePrice} <em>{productInfo.default_price}</em></p>)
+          : (<p>$ {productInfo.default_price}</p>)}
       </h3>
       <div className="overview-info description">{productInfo.description}</div>
     </ProductInformationStyle>
